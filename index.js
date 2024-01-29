e48ea7f26f5dbd58de7fdd20f44854753adbc6b9
AmirHosein Mardani <139256057+this-ILECY@users.noreply.github.com>
2024-01-29 21:20:07 +0330

    session 1 is now ok

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const http = require('http');
 const port = 3000;
-const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    fs.readFile('./index.html',(err, data)=>{
-
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.end(data);
-    })
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end("Hello there!");
 })
 
 server.listen(port,()=>{
