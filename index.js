a4a31a1697c13d7af5a4673606d0f17edbefc976
this-ILECY <this.ilecy@gmail.com>
2024-01-29 19:11:53 +0330

    second server created

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const port = 3000;
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/pain'});
-    res.end('hey Amirreza!');
+    fs.readFile('./index.html',(err, data)=>{
+
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(data);
+    })
 })
 
 server.listen(port,()=>{
