0c984841c05314eea15c20da1f16bbccdfc14392
AmirHosein Mardani <139256057+this-ILECY@users.noreply.github.com>
2024-01-29 21:20:33 +0330

    text type modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
+    res.writeHead(200,{'Content-Type':'text/text'});
     res.end("Hello there!");
 })
 
