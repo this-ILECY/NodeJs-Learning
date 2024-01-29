1d53af4029390d3845335cec4b156f02f977def1
this-ILECY <this.ilecy@gmail.com>
2024-01-29 20:41:46 +0330

    shorter res syntax + client query

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const http = require('http');
+//we have shorter version of 'res' here. check line 11 for instance!
+
+const express = require("express");
 const port = 3000;
+const app = express();
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/pain'});
-    res.end('hey Amirreza!');
-})
+app.get("/get", (req, res) => {
+  const id = req.query.id;
+  res.type("text/pain");
 
-server.listen(port,()=>{
-    
-})
+  if (id == 1) res.send("You chosed Amirhosein!");
+  else if (id == 2) res.send("You chosed Amirreza!");
+  else res.status(404).send("404 Error: id is missing")
+});
+
+//if you put 'app.get' under 'app.use', won't work.
+app.get("/amirreza", (req, res) => {
+  res.type("text/pain").send("Hello Amirreza!");
+});
+
+app.get("/amirhosein", (req, res) => {
+  res.type("text/pain").send("Hello Amirhosein!");
+});
+
+app.use((req, res) => {
+  res.type("text/pain").status(404).send("404 not found");
+});
+
+app.listen(port, () => {});
