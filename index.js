22dd68b9bba8c162d157e791b72938f62c4994d6
this-ILECY <this.ilecy@gmail.com>
2024-01-29 22:24:34 +0330

    random user with dynamic value

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { engine: expressHandlebars } = require('express-handlebars')
+const { engine: expressHandlebars } = require("express-handlebars");
 
 const app = express();
 
@@ -14,7 +14,13 @@ app.set("view engine", "handlebars");
 
 const port = 3000;
 
+const users = ["Amirhosein", "Amirreza"];
+
 app.get("/", (req, res) => res.render("home"));
 app.get("/amirreza", (req, res) => res.render("amirreza"));
+app.get("/users", (req, res) => {
+  const user = users[(Math.random()).toFixed(0)];
+  res.render("users", { userlist: user });
+});
 
 app.listen(port, () => {});
