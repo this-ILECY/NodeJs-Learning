015b2285045599b9daf1cefeb34ca1eebcd53d6d
this-ILECY <this.ilecy@gmail.com>
2024-02-01 12:47:31 +0330

    simple 'get' sample

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const http = require('http');
 const port = 3000;
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/text'});
-    res.end("Hello there!");
+let users = [
+    'Amir', 'Mohammad', 'Sarah', 'John', 'James', 'Asghar',
+]
+
+app.get('/get', (res, req) => {
+
+    const id = res.query.id;
+    if (id !== undefined) {
+        req.writeHead(200, { 'contentType': 'text/plain' });
+        req.end("the user is: " + users[id] + "\nBye there!\n");
+    }
+    else {
+        req.writeHead(200, { 'contentType': 'application/json' });
+        req.end(JSON.stringify(users));
+    }
 })
 
-server.listen(port,()=>{
-    console.log("this is console!");
+app.listen(port, () => {
+
 })
